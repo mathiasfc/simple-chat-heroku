5f8cb0043b98340f8b4dbaf6d155bbeb1cc954ab
Mathias Falci de Castro <mathias.falci@gmail.com>
2017-08-15 14:32:16 -0300

    Updated chat

--- server.js
+++ server.js
@@ -37,54 +37,64 @@ app.get('/', function(req, res){
 });*/
 //console.log("Server listening on port " + appPort);
 
-// Handle the socket.io connections
+// Chatroom
 
-var users = 0; //count the users
+var numUsers = 0;
 
-io.sockets.on('connection', function (socket) { // First connection
-	users += 1; // Add 1 to the count
-	reloadUsers(); // Send the count to all the users
-	socket.on('message', function (data) { // Broadcast the message to all
-		if(pseudoSet(socket))
-		{
-			var transmit = {date : new Date().toISOString(), pseudo : socket.nickname, message : data};
-			socket.broadcast.emit('message', transmit);
-			console.log("user "+ transmit['pseudo'] +" said \""+data+"\"");
-		}
-	});
-	socket.on('setPseudo', function (data) { // Assign a name to the user
-		if (pseudoArray.indexOf(data) == -1) // Test if the name is already taken
-		{
-			pseudoArray.push(data);
-			socket.nickname = data;
-			socket.emit('pseudoStatus', 'ok');
-			console.log("user " + data + " connected");
-		}
-		else
-		{
-			socket.emit('pseudoStatus', 'error') // Send the error
-		}
-	});
-	socket.on('disconnect', function () { // Disconnection of the client
-		users -= 1;
-		reloadUsers();
-		if (pseudoSet(socket))
-		{
-			console.log("disconnect...");
-			var pseudo;
-			pseudo = socket.nickname;
-			var index = pseudoArray.indexOf(pseudo);
-			pseudo.slice(index - 1, 1);
-		}
-	});
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
-
-function reloadUsers() { // Send the count of the users to all
-	io.sockets.emit('nbUsers', {"nb": users});
-}
-function pseudoSet(socket) { // Test if the user has a name
-	var test;
-	if (socket.nickname == null ) test = false;
-	else test = true;
-	return test;
-}
